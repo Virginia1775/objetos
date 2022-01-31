d5bc8f39bacd1698196efa8bab8e52257fc3c9cb
David Casas <davidecasas@hotmail.com>
2022-01-31 08:21:43 +0100

    Commit con la clase creada
    Se ha creado la clase Persona con sus métodos getters y setters, el
    método toString y los métodos equals y hashCode

---
+++ Objetos/src/Persona.java
@@ -0,0 +1,103 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+/**
+ * 
+ */
+
+/**
+ * @author David
+ *
+ */
+public class Persona {
+	private String nombre;
+	private String apellidos;
+	private LocalDate fechaNac;
+	private String dni;
+	
+	public Persona() {
+		this.fechaNac=LocalDate.now();
+		this.nombre="";
+		this.apellidos="";
+		this.dni="";
+	}
+	
+	public Persona(String nombre, String apellidos, String dni) {
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.dni=dni;
+		this.fechaNac=LocalDate.now();
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	
+	public void setFechaNac(int dia, int mes, int año) {
+		this.fechaNac=LocalDate.of(año, mes, dia);
+	}
+
+	public long getEdad() {
+		return this.fechaNac.until(LocalDate.now(),ChronoUnit.YEARS);
+	}
+	
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellidos=" + apellidos 
+				+ ", Edad=" + getEdad() +", dni=" + dni+"]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (obj instanceof String)
+			dni.equals((String)obj);
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(dni, other.dni);
+	}
+
+	
+	
+	
+	
+}
---
+++ Objetos/src/PruebaPersona.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author David
+ *
+ */
+public class PruebaPersona {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Persona p = new Persona();
+		p.setNombre("David");
+		p.setApellidos("Casas");
+		p.setFechaNac(3, 6, 1979);
+		p.setDni("30000000");
+		
+		Persona p2 = new Persona("Ana", "Pérez", "40000000");
+		
+		JOptionPane.showMessageDialog(null, p);
+		JOptionPane.showMessageDialog(null, p2);
+		
+		Persona p3 = new Persona("Pepe", "Gomez", "40000000");
+		if (p2.equals(p3)) {
+			JOptionPane.showMessageDialog(null, "p2 y p3 son iguales");
+		} else {
+			JOptionPane.showMessageDialog(null, "p2 y p3 son distintos");
+		}
+		
+
+	}
+
+}
